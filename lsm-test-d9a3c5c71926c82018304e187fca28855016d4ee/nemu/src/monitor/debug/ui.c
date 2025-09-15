e2f72475f426a0c7b337183d32b2179e9cfb4db8
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-15 09:42:21 +0000

    >  compile NEMU 3023244286 yangzihao Linux af46f5cc0739 6.10.14-linuxkit #1 SMP Wed Sep  3 15:35:15 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:42:21 up  1:12,  0 users,  load average: 0.33, 0.19, 0.20 672ecdb2f7c0ee465efc1e79e648ee0fb721d2c8

---
+++ lsm-test-d9a3c5c71926c82018304e187fca28855016d4ee/nemu/src/monitor/debug/ui.c
@@ -0,0 +1,175 @@
+#include "monitor/monitor.h"
+#include "monitor/expr.h"
+#include "monitor/watchpoint.h"
+#include "nemu.h"
+
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+void cpu_exec(uint32_t);
+void display_reg();
+
+/* We use the `readline' library to provide more flexibility to read from stdin. */
+char* rl_gets() {
+	static char *line_read = NULL;
+
+	if (line_read) {
+		free(line_read);
+		line_read = NULL;
+	}
+
+	line_read = readline("(nemu) ");
+
+	if (line_read && *line_read) {
+		add_history(line_read);
+	}
+
+	return line_read;
+}
+int nr_exp=0,nr_wp=0;
+/* TODO: Add single step */
+static int cmd_si(char *args) {
+	char *arg = strtok(NULL, " ");
+	int i = 1;
+
+	if(arg != NULL) {
+		sscanf(arg, "%d", &i);
+	}
+	cpu_exec(i);
+	return 0;
+}
+
+/* TODO: Add info command */
+static int cmd_info(char *args){
+	char *sencondWord = strtok(NULL," ");
+	int i;
+	if (strcmp(sencondWord, "r") == 0){
+		for (i = 0; i < 8; i++){
+			printf("%s\t\t", regsl[i]);
+			printf("0x%08x\t\t%d\n", cpu.gpr[i]._32, cpu.gpr[i]._32);
+		}
+		printf("eip\t\t0x%08x\t\t%d\n", cpu.eip, cpu.eip);
+	return 0;
+	}
+	printf("MISINPUT\n");
+	return 0;
+}
+
+/* Add examine memory */
+static int cmd_x(char *args){
+	int len = 0;
+	char exp[1024];
+	if(args==NULL || sscanf(args, "%d %[^\n]", &len, exp)!=2){
+		printf("Invalid Arguments %s.\n",args);
+		return 0;
+	}
+	bool success = true;
+	swaddr_t addr = expr(exp, &success);
+	if(!success){
+		printf("Invalid Address %s.\n",exp);
+		return 0;
+	}
+	int i;
+	for(i = 0;i < len; i++){
+		printf("0x%08x:\t\t0x%08x\n", addr + i * 4, swaddr_read(addr + i * 4, 4));
+	}
+	return 0;
+}
+static int cmd_p(char *args){
+ bool success=true;
+ uint32_t val =expr(args,&success);//调用expr函数
+ if(success){
+	printf("0x%x (%u)\n",val,val);
+
+ }
+ return 0;
+}
+
+
+static int cmd_c(char *args) {
+	cpu_exec(-1);
+	return 0;
+}
+
+static int cmd_q(char *args) {
+	return -1;
+}
+
+static int cmd_help(char *args);
+
+static struct {
+	char *name;
+	char *description;
+	int (*handler) (char *);
+} cmd_table [] = {
+	{ "help", "Display informations about all supported commands", cmd_help },
+	{ "c", "Continue the execution of the program", cmd_c },
+	{ "q", "Exit NEMU", cmd_q }, 
+
+	/* TODO: Add more commands */
+        { "si", "Single step", cmd_si },
+        { "info", "info r - print register values; info w - show watch point state", cmd_info },
+	    { "x", "Examine memory", cmd_x },
+		{ "p", "Print value of the expression", cmd_p}, 
+};
+
+#define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
+
+static int cmd_help(char *args) {
+	/* extract the first argument */
+	char *arg = strtok(NULL, " ");
+	int i;
+
+	if(arg == NULL) {
+		/* no argument given */
+		for(i = 0; i < NR_CMD; i ++) {
+			printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+		}
+	}
+	else {
+		for(i = 0; i < NR_CMD; i ++) {
+			if(strcmp(arg, cmd_table[i].name) == 0) {
+				printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+				return 0;
+			}
+		}
+		printf("Unknown command '%s'\n", arg);
+	}
+	return 0;
+}
+
+void ui_mainloop() {
+	while(1) {
+		char *str = rl_gets();
+		char *str_end = str + strlen(str);
+
+		/* extract the first token as the command */
+		char *cmd = strtok(str, " ");
+		if(cmd == NULL) { continue; }
+
+		/* treat the remaining string as the arguments,
+		 * which may need further parsing
+		 */
+		char *args = cmd + strlen(cmd) + 1;
+		if(args >= str_end) {
+			args = NULL;
+		}
+
+#ifdef HAS_DEVICE
+		extern void sdl_clear_event_queue(void);
+		sdl_clear_event_queue();
+#endif
+
+		int i;
+		for(i = 0; i < NR_CMD; i ++) {
+			if(strcmp(cmd, cmd_table[i].name) == 0) {
+				if(cmd_table[i].handler(args) < 0) { return; }
+				break;
+			}
+		}
+
+		if(i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
+	}
+}
+
