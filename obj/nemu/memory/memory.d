b7636248a1414f32ffdd6e2912d7d1cf5ed4cf36
3023005041-yangzihao <yangzihao_41@outlook.com>
2025-09-15 09:59:19 +0000

    pa1

--- obj/nemu/memory/memory.d
+++
@@ -1,5 +0,0 @@
-obj/nemu/memory/memory.o: nemu/src/memory/memory.c nemu/include/common.h \
- nemu/include/debug.h nemu/include/macro.h nemu/src/memory/burst.h \
- nemu/include/memory/tlb.h nemu/include/memory/cache.h \
- nemu/include/cpu/reg.h nemu/include/../../lib-common/x86-inc/cpu.h \
- nemu/include/../../lib-common/x86-inc/mmu.h
--- obj/nemu/memory/tlb.d
+++
@@ -1,3 +0,0 @@
-obj/nemu/memory/tlb.o: nemu/src/memory/tlb.c nemu/include/memory/tlb.h \
- nemu/include/common.h nemu/include/debug.h nemu/include/macro.h \
- nemu/src/memory/burst.h
