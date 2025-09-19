45f77ac871e3dede03cab13e7809bace5c42750a
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-19 16:49:16 +0000

    >  compile NEMU 3023005041 user Linux 28797137d156 6.10.14-linuxkit #1 SMP Wed Sep  3 15:35:15 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:49:16 up 33 min,  0 users,  load average: 0.22, 0.25, 0.25 f1210d60ac17644d50d4fd4d827c7dde6ef78dea

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -5,7 +5,7 @@
 static void do_execute() {
 	cpu.esp -= 4;
 	// push next instr address
-	swaddr_write(cpu.esp, DATA_BYTE, cpu.eip + 1 + DATA_BYTE);
+	swaddr_write(cpu.esp, 4, cpu.eip + 1 + DATA_BYTE);
 	cpu.eip = cpu.eip + 1 + DATA_BYTE + op_src->simm;
 	print_asm_template1();
 }
---
+++ nemu/src/cpu/exec/control/ret.c
@@ -0,0 +1,10 @@
+#include "cpu/exec/helper.h"
+
+make_helper(ret) {
+	//pop caller next instr address
+	swaddr_t addr = swaddr_read(cpu.esp, 4);
+	cpu.eip = addr;
+	cpu.esp += 4;
+	print_asm("ret");
+	return 1;
+}
