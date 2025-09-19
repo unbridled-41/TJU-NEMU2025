8749f676d1e3f9fdf066f35d022296225a5ac2cb
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-19 16:44:53 +0000

    >  compile NEMU 3023005041 user Linux 28797137d156 6.10.14-linuxkit #1 SMP Wed Sep  3 15:35:15 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:44:53 up 29 min,  0 users,  load average: 0.61, 0.41, 0.30 496f222543eab6886dc01838e464ac89b3a564d9

--- nemu/src/cpu/exec/data-mov/pop-template.h
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -3,13 +3,6 @@
 #define instr pop
 
 static void do_execute() {
-	uint32_t val = swaddr_read(cpu.esp, DATA_BYTE);
-	if(op_src->type == OP_TYPE_REG)
-		REG(op_src->reg) = val;
-	else if(op_src->type == OP_TYPE_MEM)
-	      swaddr_write(op_src->addr,DATA_BYTE, val);
-	else 
-	      assert(0);
     OPERAND_W(op_src, MEM_R(cpu.esp));
 	cpu.esp += DATA_BYTE;
 	print_asm_template1();
