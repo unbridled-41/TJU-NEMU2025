586a9c8c9796f1106eb1c1643ea65523e78cf179
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-19 08:26:23 +0000

    >  compile NEMU 3023005041 user Linux 28797137d156 6.10.14-linuxkit #1 SMP Wed Sep  3 15:35:15 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  08:26:23 up 13 min,  0 users,  load average: 0.60, 0.27, 0.12 b685dce77c88f88c1e91bc1fe880268028edf729

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -4,6 +4,8 @@
 #include "data-mov/xchg.h"
 #include "data-mov/movext.h"
 #include "data-mov/cltd.h"
+#include "data-mov/push.h"
+#include "data-mov/pop.h"
 
 #include "arith/adc.h"
 #include "arith/dec.h"
