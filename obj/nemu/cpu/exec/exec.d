3d3e03ba9a4a9a24e89bb8e9cbc87f28ac914149
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-19 16:42:27 +0000

    >  compile NEMU 3023005041 user Linux 28797137d156 6.10.14-linuxkit #1 SMP Wed Sep  3 15:35:15 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:42:27 up 27 min,  0 users,  load average: 0.54, 0.38, 0.29 1a94d107160bd4fb83694ef1eac4627bfb7aeca

--- obj/nemu/cpu/exec/exec.d
+++ obj/nemu/cpu/exec/exec.d
@@ -13,10 +13,11 @@ obj/nemu/cpu/exec/exec.o: nemu/src/cpu/exec/exec.c \
  nemu/src/cpu/exec/arith/mul.h nemu/src/cpu/exec/arith/idiv.h \
  nemu/src/cpu/exec/arith/div.h nemu/src/cpu/exec/arith/sbb.h \
  nemu/src/cpu/exec/arith/sub.h nemu/src/cpu/exec/control/jmp.h \
- nemu/src/cpu/exec/logic/and.h nemu/src/cpu/exec/logic/or.h \
- nemu/src/cpu/exec/logic/not.h nemu/src/cpu/exec/logic/xor.h \
- nemu/src/cpu/exec/logic/sar.h nemu/src/cpu/exec/logic/shl.h \
- nemu/src/cpu/exec/logic/shr.h nemu/src/cpu/exec/logic/shrd.h \
- nemu/src/cpu/exec/string/rep.h nemu/src/cpu/exec/string/scas.h \
- nemu/src/cpu/exec/string/stos.h nemu/src/cpu/exec/string/movs.h \
- nemu/src/cpu/exec/misc/misc.h nemu/src/cpu/exec/special/special.h
+ nemu/src/cpu/exec/control/call.h nemu/src/cpu/exec/logic/and.h \
+ nemu/src/cpu/exec/logic/or.h nemu/src/cpu/exec/logic/not.h \
+ nemu/src/cpu/exec/logic/xor.h nemu/src/cpu/exec/logic/sar.h \
+ nemu/src/cpu/exec/logic/shl.h nemu/src/cpu/exec/logic/shr.h \
+ nemu/src/cpu/exec/logic/shrd.h nemu/src/cpu/exec/string/rep.h \
+ nemu/src/cpu/exec/string/scas.h nemu/src/cpu/exec/string/stos.h \
+ nemu/src/cpu/exec/string/movs.h nemu/src/cpu/exec/misc/misc.h \
+ nemu/src/cpu/exec/special/special.h
