c74cf8e8137bac366e5aa8add7e2c13eda259d9f
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-20 07:25:08 +0000

    >  compile NEMU 3023005041 user Linux 28797137d156 6.10.14-linuxkit #1 SMP Wed Sep  3 15:35:15 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  07:25:08 up  3:19,  0 users,  load average: 0.62, 0.29, 0.21 764ddace4a713be0a17b2a131f4d71b444520b

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -6,7 +6,7 @@ static void do_execute() {
 	cpu.esp -= 4;
 	// push next instr address
 	swaddr_write(cpu.esp, 4, cpu.eip + 1 + DATA_BYTE);
-	cpu.eip = cpu.eip + 1 + DATA_BYTE + op_src->simm;
+	cpu.eip += op_src->simm;
 	print_asm_template1();
 }
 
