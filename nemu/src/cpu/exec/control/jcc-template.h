980a88a280835c3a6f98ebc6211d013635cdfba5
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-21 12:10:38 +0000

    >  compile NEMU 3023005041 user Linux 28797137d156 6.10.14-linuxkit #1 SMP Wed Sep  3 15:35:15 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:10:38 up  6:34,  0 users,  load average: 0.19, 0.28, 0.17 eaba17061323ad6015697f9f8137c951bfa77f55

--- nemu/src/cpu/exec/control/jcc-template.h
+++ nemu/src/cpu/exec/control/jcc-template.h
@@ -1,16 +1,48 @@
 #include "cpu/exec/template-start.h"
 
-#define cond e
+#if DATA_BYTE == 1
 
-static void concat4(do_j, cond, _, SUFFIX)(){
-	if(!(cpu.eflags.ZF == 1))return ;
-	cpu.eip += op_src->val;
-	print_asm("j" str(cond) " %x", cpu.eip + 1 + DATA_BYTE);}
+#define ZF (cpu.eflags.ZF)
+#define SF (cpu.eflags.SF)
+#define CF (cpu.eflags.CF)
+#define OF (cpu.eflags.OF)
+#define PF (cpu.eflags.PF)
+#endif
 
+#define make_jcc_helper(cond, name) \
+	static void concat4(do_j, name, _, SUFFIX)(){ \
+	if(!(cond))return ; \
+	cpu.eip += op_src->val; \
+	print_asm("j" str(name) " %x", cpu.eip + 1 + DATA_BYTE); \
+} \
+make_helper(concat4(j, name, _, SUFFIX)){ \
+	return idex(eip, concat(decode_si_, SUFFIX), concat4(do_j, name, _, SUFFIX)); \
+} 
 
-make_helper(concat4(j, cond, _, SUFFIX)){
-	return idex(eip, concat(decode_si_, SUFFIX), concat4(do_j, cond, _, SUFFIX));
-}
+make_jcc_helper(!CF&!ZF,a)
+make_jcc_helper(!CF,ae)
+make_jcc_helper(CF,b)
+make_jcc_helper(CF|ZF,be)
+make_jcc_helper(!cpu.ecx,ecxz)
+make_jcc_helper(ZF,e)
+make_jcc_helper(!ZF&(SF==OF),g)
+make_jcc_helper(ZF|(SF==OF),ge)
+make_jcc_helper(SF!=OF,l)
+make_jcc_helper(ZF|(SF!=OF),le)
+make_jcc_helper(!ZF,ne)
+make_jcc_helper(!OF,no)	
+make_jcc_helper(!PF,np)
+make_jcc_helper(!SF,ns)
+make_jcc_helper(OF,o)
+make_jcc_helper(PF,p)
+make_jcc_helper(SF,s)
+#if DATA_BYTE == 4
 
+#undef ZF
+#undef SF
+#undef CF
+#undef OF
 
-#include "cpu/exec/template-end.h"
+#endif
+
+#include "cpu/exec/template-end.h" 
