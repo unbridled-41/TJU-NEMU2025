dbcbe304e95b0fc0b49023f19a78c22d45b9720e
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-21 12:24:06 +0000

    >  compile NEMU 3023005041 user Linux 28797137d156 6.10.14-linuxkit #1 SMP Wed Sep  3 15:35:15 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:24:06 up  6:47,  0 users,  load average: 0.22, 0.20, 0.18 594246e66d6cdded33f15d56baaf08aaa778a2ed

--- nemu/src/cpu/exec/control/jcc.c
+++ nemu/src/cpu/exec/control/jcc.c
@@ -12,4 +12,20 @@
 #include "jcc-template.h"   
 #undef DATA_BYTE
 
+make_helper_v(ja)
+make_helper_v(jae)
+make_helper_v(jb)
+make_helper_v(jbe)
+make_helper_v(jecxz)
 make_helper_v(je)
+make_helper_v(jg)
+make_helper_v(jge)
+make_helper_v(jl)
+make_helper_v(jle)
+make_helper_v(jne)
+make_helper_v(jno)	
+make_helper_v(jnp)
+make_helper_v(jns)
+make_helper_v(jo)
+make_helper_v(jp)
+make_helper_v(js)
