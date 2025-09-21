56f4b33b50702a3592c8770b5c225690a386cdf2
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-21 12:07:22 +0000

    >  compile NEMU 3023005041 user Linux 28797137d156 6.10.14-linuxkit #1 SMP Wed Sep  3 15:35:15 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:07:22 up  6:31,  0 users,  load average: 0.08, 0.14, 0.10 f2f8b826bbd518aab716d87282e4b6b783a8b9c

--- nemu/src/cpu/exec/control/jcc-template.h
+++ nemu/src/cpu/exec/control/jcc-template.h
@@ -1,15 +1,15 @@
 #include "cpu/exec/template-start.h"
 
-#if DATA_BYTE == 1
-static void do_je(){
+#define cond e
+
+static void concat4(do_j, cond, _, SUFFIX)(){
 	if(!(cpu.eflags.ZF == 1))return ;
 	cpu.eip += op_src->val;
-	print_asm(str(instr) " %x", cpu.eip + 1 + DATA_BYTE);
-}
-#endif
+	print_asm("j" str(cond) " %x", cpu.eip + 1 + DATA_BYTE);}
 
-make_helper(concat(je_,SUFFIX)){
-	return idex(eip, concat(decode_si_, SUFFIX), concat(do_, je));
+
+make_helper(concat4(j, cond, _, SUFFIX)){
+	return idex(eip, concat(decode_si_, SUFFIX), concat4(do_j, cond, _, SUFFIX));
 }
 
 
