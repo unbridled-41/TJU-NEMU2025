72d0a1ee344df4908459fc3061b1d789df0273d5
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-24 07:26:40 +0000

    >  compile NEMU 3023005041 user Linux 28797137d156 6.10.14-linuxkit #1 SMP Wed Sep  3 15:35:15 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  07:26:40 up  4:25,  0 users,  load average: 0.27, 0.23, 0.19 74a8ca8ee73ff09571ec460d1dd79c1516ebddee

--- obj/nemu/cpu/exec/exec.d
+++ obj/nemu/cpu/exec/exec.d
@@ -23,4 +23,5 @@ obj/nemu/cpu/exec/exec.o: nemu/src/cpu/exec/exec.c \
  nemu/src/cpu/exec/logic/test.h nemu/src/cpu/exec/logic/setcc.h \
  nemu/src/cpu/exec/string/rep.h nemu/src/cpu/exec/string/scas.h \
  nemu/src/cpu/exec/string/stos.h nemu/src/cpu/exec/string/movs.h \
- nemu/src/cpu/exec/misc/misc.h nemu/src/cpu/exec/special/special.h
+ nemu/src/cpu/exec/string/lods.h nemu/src/cpu/exec/misc/misc.h \
+ nemu/src/cpu/exec/special/special.h
