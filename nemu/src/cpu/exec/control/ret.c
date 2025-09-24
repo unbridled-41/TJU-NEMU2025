fa844abdc9d68a6d8a3b4d1788db8e672a0b733b
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-24 07:14:08 +0000

    >  compile NEMU 3023005041 user Linux 28797137d156 6.10.14-linuxkit #1 SMP Wed Sep  3 15:35:15 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  07:14:08 up  4:13,  0 users,  load average: 0.05, 0.14, 0.14 40ab4538bab8e4b1d181e51fd1a398a55b939c7d

--- nemu/src/cpu/exec/control/ret.c
+++ nemu/src/cpu/exec/control/ret.c
@@ -1,11 +1,22 @@
 #include "cpu/exec/helper.h"
-
+ 
 make_helper(ret) {
 	//pop caller next instr address
 	swaddr_t addr = swaddr_read(cpu.esp, 4);
-	cpu.eip = addr;
+	cpu.eip = addr - 1;
 	cpu.esp += 4;
 	print_asm("ret");
-	//  ret 0 to cpu-exec
-	return 0;
+	return 1;
+}
+
+make_helper(ret_i_w) {
+	//pop caller next instr address
+	swaddr_t addr = swaddr_read(cpu.esp, 4);
+	cpu.eip = addr - 1;
+	cpu.esp += 4;
+	// pop imm16 bytes
+	int len = decode_i_w(eip + 1);
+	cpu.esp += 4*(op_src->val);
+	print_asm_template1();
+	return len + 1;
 }
