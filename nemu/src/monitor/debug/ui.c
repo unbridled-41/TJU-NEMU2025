2f9d326225aac8ccc5aaa7a60fa55935954480b1
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-25 11:48:12 +0000

    >  compile NEMU 3023005041 user Linux 28797137d156 6.10.14-linuxkit #1 SMP Wed Sep  3 15:35:15 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:48:11 up  7:30,  0 users,  load average: 0.20, 0.23, 0.24 52afbee8a78100fbddcf57030e49f7e72ba5c73

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -211,18 +211,20 @@ static int cmd_bt(char *args){
 	}
 	else{
 		swaddr_t ebp_now = cpu.ebp;
-		swaddr_t ebp_last = 0;
 		swaddr_t ret_addr = 0;
+		char *func_name = NULL;
 		int nr_frame = 1;
 		printf("#0 %s arg:(", in_which_func(cpu.eip));
 		printf(")\n");
 		while(ebp_now) {
-			ebp_last = swaddr_read(ebp_now, 4);
 			ret_addr = swaddr_read(ebp_now + 4, 4);
-			printf("#%d 0x%08x in %s arg:(", nr_frame, ret_addr, in_which_func(ret_addr));
+			func_name = in_which_func(ret_addr);
+			if(!func_name)break;
+			printf("#%d 0x%08x in %s arg:(", nr_frame, ret_addr, func_name);
 			printf(")\n");
 			nr_frame ++;
-			ebp_now = ebp_last;
+			ebp_now = swaddr_read(ebp_now, 4);
+			
 		}
 	}	
 	return 0;
