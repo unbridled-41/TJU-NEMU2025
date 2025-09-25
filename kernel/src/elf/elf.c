6462252a91f05869b7bd8ce6087bd1acb70f7fe1
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-25 12:12:40 +0000

    >  compile kernel 3023005041 user Linux 28797137d156 6.10.14-linuxkit #1 SMP Wed Sep  3 15:35:15 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:12:40 up  7:55,  0 users,  load average: 0.19, 0.19, 0.18 ffc63dde95db8744ae01985a820547d9cbf952c0

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -9,12 +9,14 @@
 void ide_read(uint8_t *, uint32_t, uint32_t);
 #else
 void ramdisk_read(uint8_t *, uint32_t, uint32_t);
+void ramdisk_write(uint8_t *buf, uint32_t offset, uint32_t len);
 #endif
 
 #define STACK_SIZE (1 << 20)
 
 void create_video_mapping();
 uint32_t get_ucr3();
+
 
 uint32_t loader() {
 	Elf32_Ehdr *elf;
@@ -31,33 +33,36 @@ uint32_t loader() {
 	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
-	const uint32_t elf_magic = 0xBadC0de;
+	const uint32_t elf_magic = 0x7f454c46;;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
 
 	/* Load each program segment */
-	panic("please implement me");
-	for(; true; ) {
+//	panic("please implement me");
+	ph = (void *)(buf + elf->e_phoff);
+	int i = 0;
+	for(i = 0; i < elf->e_phnum; i++) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
 
+			uint8_t membuf[4096];
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			 
-			 
+			ramdisk_read(membuf, ph->p_offset, ph->p_filesz);
+			ramdisk_write(membuf, ph->p_vaddr, ph->p_filesz);
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-
-
+            uint8_t zerobuf[4096] = {0};
+			ramdisk_write(zerobuf, ph->p_vaddr + ph->p_filesz, ph->p_memsz - ph->p_filesz);
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
 			extern uint32_t cur_brk, max_brk;
 			uint32_t new_brk = ph->p_vaddr + ph->p_memsz - 1;
 			if(cur_brk < new_brk) { max_brk = cur_brk = new_brk; }
 #endif
-		}
+		}ph ++;
 	}
 
 	volatile uint32_t entry = elf->e_entry;
