733f20d11a64615910bba377abbc0328c26adcf2
tracer-NEMU2020 <tjuics2020@163.com>
2025-10-05 08:07:05 +0000

    >  compile NEMU 3023005041 user Linux 28797137d156 6.10.14-linuxkit #1 SMP Wed Sep 10 06:47:45 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  08:07:05 up  4:42,  0 users,  load average: 0.51, 0.27, 0.17 d8d23251ee0b3d2850abce32bea0dee08542b27f

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,12 +1,22 @@
-#include "common.h"
-
+//#include "common.h"
+#include "nemu.h"
 uint32_t cache_read(hwaddr_t, size_t);
 void cache_write(hwaddr_t, size_t, uint32_t);
+uint32_t dram_read(hwaddr_t, size_t);
+void dram_write(hwaddr_t, size_t, uint32_t);
+
 
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-	return cache_read(addr, len) & (~0u >> ((4 - len) << 3));
+    uint32_t dr = dram_read(addr, len);
+	uint32_t cr = cache_read(addr,len);
+	if(dr!=cr){
+		printf("0x%x,eip=0x%x\n",addr,cpu.eip);
+		printf("dr = 0x%x\ncr = 0x%x\n",dr,cr);
+	}	
+	return cr & (~0u >> ((4 - len) << 3));
+    //	return cache_read(addr, len) & (~0u >> ((4 - len) << 3));
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
--- obj/nemu/memory/memory.d
+++ obj/nemu/memory/memory.d
@@ -1,2 +1,4 @@
-obj/nemu/memory/memory.o: nemu/src/memory/memory.c nemu/include/common.h \
- nemu/include/debug.h nemu/include/macro.h
+obj/nemu/memory/memory.o: nemu/src/memory/memory.c nemu/include/nemu.h \
+ nemu/include/common.h nemu/include/debug.h nemu/include/macro.h \
+ nemu/include/memory/memory.h nemu/include/common.h \
+ nemu/include/cpu/reg.h
