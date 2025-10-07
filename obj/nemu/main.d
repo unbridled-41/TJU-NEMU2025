9b6ed2ac55dc94a3710199a17d530fecf6619890
杨子豪 <yangzihao@yangzihaodeMacBook-Air.local>
2025-10-07 16:33:57 +0800

    'Add Git LFS tracking for large files'

--- obj/nemu/main.d
+++
@@ -1,4 +0,0 @@
-obj/nemu/main.o: nemu/src/main.c nemu/include/nemu.h \
- nemu/include/common.h nemu/include/debug.h nemu/include/macro.h \
- nemu/include/memory/memory.h nemu/include/common.h \
- nemu/include/cpu/reg.h
--- obj/nemu/memory/memory.d
+++
@@ -1,4 +0,0 @@
-obj/nemu/memory/memory.o: nemu/src/memory/memory.c nemu/include/nemu.h \
- nemu/include/common.h nemu/include/debug.h nemu/include/macro.h \
- nemu/include/memory/memory.h nemu/include/common.h \
- nemu/include/cpu/reg.h
